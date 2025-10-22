8476c4209ca4e81dfde20f025d849de3800bd8b5
Erin Cho <echo@crc-dot1x-nat-10-239-105-199.bu.edu>
2025-10-22 19:43:08 -0400

    Uploaded profile pic

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -19,7 +19,7 @@ const StyledDiv = styled.div`
 `;
 
 const StyledImg = styled.img`
-    width: 40%;
+    width: 55%;
 `;
 
 export default function Home() {
@@ -33,14 +33,17 @@ export default function Home() {
                     <h3 id="main-title"> Home </h3>
                     <div id="main-image"><StyledImg src={profilePic} alt="Erin Cho"/></div>
 
-                        <p>     Hi! My name is Erin a</p>
+                        <p>     Hi, I'm Erin! I'm an enthusiastic and passionate
+                            senior at Boston University majoring in Computer Science and
+                            minoring in Educational Public Policy. Currently, I serve as a
+                            Peer Tutor for the College of Arts & Sciences Core Curriculum, and
+                            as a high school tutor, specializing in Algebra and Geometry. </p>
 
-                        <p>     Chain keep us together (running in the shadow)
-                                Chain keep us together (running in the shadow)
-                                Chain keep us together (running in the shadow)
-                                Chain keep us together (running in the shadow)
-                                Chain keep us together (running in the shadow)
-                                Chain
+                        <p>     On top of my studies and supporting-education roles, I'm also
+                            very excited about second-hand fashion, fabric crafts, and jewelry-making.
+                            I love sharing my insights and my work to those around me, and I plan on
+                            carrying these passions with me throughout my professional life!
+
                         </p>
                 </StyledDiv>
             </StyledPage>
