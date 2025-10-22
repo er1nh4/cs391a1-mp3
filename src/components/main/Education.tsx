8da6580ab6b808971dbd8071e22b7d3d94e14c1d
Erin Cho <echo@crc-dot1x-nat-10-239-105-199.bu.edu>
2025-10-22 15:31:39 -0400

    Updated htmls

--- src/components/main/Education.tsx
+++ src/components/main/Education.tsx
@@ -5,18 +5,12 @@ const StyledPage = styled.main`
     height: 100vh;
     display: flex;
     flex-direction: column;
+    padding: 0 5%;
 `;
 
 const StyledDiv = styled.div`
-    padding: 0 5%;
     text-align: center;
     font-family: "Arial Rounded MT Bold", Arial, san-serif, serif;
-    
-    @media screen and (max-width: 700px) {
-        padding: 0;
-        align-items: center;
-        justify-content: center;
-    }
 `;
 
 export default function Education() {
--- src/components/main/Employment.tsx
+++ src/components/main/Employment.tsx
@@ -5,18 +5,20 @@ const StyledPage = styled.main`
     height: 100vh;
     display: flex;
     flex-direction: column;
+    padding: 0 5%;
+`;
 
-    @media screen and (max-width: 700px) {
-        padding: 0;
-        align-items: center;
-        justify-content: center;
-    }
+const StyledDiv = styled.div`
+    text-align: center;
+    font-family: "Arial Rounded MT Bold", Arial, san-serif, serif;
 `;
 
 export default function Employment() {
     return (
         <StyledPage id="main-page">
-            <h3 id="main-title"> Employment History </h3>
+            <StyledDiv>
+                <h3 id="main-title"> Employment History </h3>
+            </StyledDiv>
         </StyledPage>
     )
 }
