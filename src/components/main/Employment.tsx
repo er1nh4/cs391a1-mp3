b68125b91db0cbfd81c9bd935ef33c628c681ada
Erin Cho <echo@crc-dot1x-nat-10-239-105-199.bu.edu>
2025-10-22 20:12:48 -0400

    Added work exp

--- src/components/main/Employment.tsx
+++ src/components/main/Employment.tsx
@@ -6,26 +6,30 @@ const StyledPage = styled.main`
     display: flex;
     flex-direction: column;
     padding: 0 5%;
-    text-align: center;
 `;
 
 const StyledDiv = styled.div`
-    text-align: left;
+    text-align: center;
     font-family: "Arial Rounded MT Bold", Arial, san-serif, serif;
     color: #3e696a;
+`;
+
+const StyledHeader = styled.div`
+    text-align: left;
 `;
 
 export default function Employment() {
     return (
         <StyledPage id="main-page">
-            <h2 id="main-title"> Employment History </h2>
             <StyledDiv>
+                <StyledHeader>
                     <h3>Match Charter Public High School</h3>
                     <h4> Face-To-Face Tutor <em> September 2025 - Present </em></h4>
                     <p> On top of my studies and supporting-education roles, I'm also
                         very excited about second-hand fashion, fabric crafts, and jewelry-making.
                         I love sharing my insights and my work to those around me, and I plan on
                         carrying these passions with me throughout my professional life!</p>
+                </StyledHeader>
             </StyledDiv>
         </StyledPage>
     )
