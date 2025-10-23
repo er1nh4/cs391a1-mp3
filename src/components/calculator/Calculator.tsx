8446cacf9deb63f97f658ca61d48e7fa854478c0
Erin Cho <echo@crc-dot1x-nat-10-239-105-199.bu.edu>
2025-10-22 23:51:14 -0400

    Updated formatting and got rid of spaces

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -18,19 +18,19 @@ export default function Calculator() {
 
     return (
         <>
-            <input type="num" value={num1} onChange={(e) => setNum1(e.target.value)} />
-            <input type="num" value={num2} onChange={(e) => setNum2(e.target.value)} />
+            <input type="num" value={num1} onChange={(e) => setNum1(e.target.value)}/>
+            <input type="num" value={num2} onChange={(e) => setNum2(e.target.value)}/>
 
-            <div>
-                <button onClick={doAdd}>+</button>
-                <button onClick={doSubtract}>-</button>
-                <button onClick={doMultiply}>*</button>
-                <button onClick={doDivide}>/</button>
-                <button onClick={doPow}>**</button>
-                <button onClick={clear}>clear</button>
+            <div id="buttons">
+                <button onClick={doAdd}> + </button>
+                <button onClick={doSubtract}> - </button>
+                <button onClick={doMultiply}> * </button>
+                <button onClick={doDivide}> / </button>
+                <button onClick={doPow}> ** </button>
+                <button onClick={clear}> clear </button>
             </div>
 
-            <h3 style={{color}}>{output}</h3>
+            <h3 style={{color}}> {output} </h3>
         </>
     )
 }
