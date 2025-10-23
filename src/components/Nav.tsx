155a6b0093b6960f01b808c6af0c48bc64d2021c
Erin Cho <echo@crc-dot1x-nat-10-239-105-199.bu.edu>
2025-10-22 21:23:59 -0400

    Update resume

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const StyledNav = styled.nav`
     width: 30%;
+    height: 100%;
     background-color: lightblue;
     border-right: 1px solid #3e696a;
     
--- src/components/main/Employment.tsx
+++ src/components/main/Employment.tsx
@@ -38,41 +38,61 @@ export default function Employment() {
                     <StyledList>
                         <h3>Match Charter Public High School — Face To Face Tutor</h3>
                         <h4><em> September 2025 - Present </em></h4>
-                        <p> ● Served as a junior teacher for elementary and middle school
-                            students attending introductory coding camp in Python. <br/>
-                            ● Assisted the head teacher with lesson plan development in
-                            the first 2 weeks and implemented appropriate Computer Science
-                            course-load for students in grades 4-8. <br/>
-                            ● Engaged in 1:1 sessions with parents to effectively
-                            communicate student feedback and provide details for the
-                            child's continued success based on their work samples and
-                            completed projects. <br/>
-                            ● Provided additional educator support for teachers in
-                            SHSAT/SAT exam prep in response to peak student enrollment.</p>
+                        <p> ● Lead small-group and 1:1 tutorials for 9th/10th grade
+                                mathematics (Algebra 1, Geometry), foundational math skills,
+                                and credit recovery.<br/>
+                            ● Provide executive functioning support for students using various,
+                                effective strategies, in order to strengthen their attention span,
+                                material retention, and time management skills.<br/>
+                            ● Communicate with parents and guardians regarding my students' in-school
+                                behavior, academic performance, and student-support inquiries.<br/> </p>
                     </StyledList>
                     <StyledList>
                         <h3>New York Tutoring Center — Coding Instructor</h3>
                         <h4><em> May - July 2025 </em></h4>
-                        <p> On top of my studies and supporting-education roles, I'm also
-                            very excited about second-hand fashion, fabric crafts, and jewelry-making.
-                            I love sharing my insights and my work to those around me, and I plan on
-                            carrying these passions with me throughout my professional life!</p>
+                        <p> ● Served as a junior teacher for elementary and middle school
+                                students attending introductory coding camp in Python. <br/>
+                            ● Assisted the head teacher with lesson plan development in
+                                the first 2 weeks and implemented appropriate Computer Science
+                            course-load for students in grades 4-8. <br/>
+                            ● Engaged in 1:1 sessions with parents to effectively
+                                communicate student feedback and provide details for the
+                                child's continued success based on their work samples and
+                                completed projects. <br/>
+                            ● Provided additional educator support for teachers in
+                                SHSAT/SAT exam prep in response to peak student enrollment.</p>
                     </StyledList>
                     <StyledList>
                         <h3>Google — Software Engineering Intern, STEP</h3>
                         <h4><em> May - August 2024 </em></h4>
-                        <p> On top of my studies and supporting-education roles, I'm also
-                            very excited about second-hand fashion, fabric crafts, and jewelry-making.
-                            I love sharing my insights and my work to those around me, and I plan on
-                            carrying these passions with me throughout my professional life!</p>
+                        <p> ● Engineered a secure alerting service to support Google Web
+                                Search EngProd’s continuous build integration tests,
+                                replacing deprecated infrastructure to ensure stability
+                                and timely fault detection.<br/>
+                            ● Built a full-stack microservice including server generation
+                                and backend logic in GoLang, along with unit testing for
+                                production-readiness.<br/>
+                            ● Diagnosed and resolved role-based access bugs that previously
+                                allowed unvetted code to run in integration
+                                environments, helping safeguard against potential
+                                vulnerabilities. <br/>
+                            ● Leveraged internal learning platforms (e.g., Grow C++ @ Google)
+                                and collaborated with mentors and peers through structured
+                                development programs.</p>
                     </StyledList>
                     <StyledList>
                         <h3>YouTube — Software Engineering Intern, STEP</h3>
                         <h4><em> June - September 2023 </em></h4>
-                        <p> On top of my studies and supporting-education roles, I'm also
-                            very excited about second-hand fashion, fabric crafts, and jewelry-making.
-                            I love sharing my insights and my work to those around me, and I plan on
-                            carrying these passions with me throughout my professional life!</p>
+                        <p> ● Delivered a high-impact feature, Variable Timeout, supporting
+                                user moderation for creators and privileged users across the
+                                YouTube Live Chat experience.<br/>
+                            ● Completed cross-stack development using internal UI frameworks
+                                and backend systems in C++, including full product lifecycle:
+                                from design doc, UX mock approvals, to a team-wide bug bash to
+                                ensure secure, stable feature deployment. <br/>
+                            ● Helped unblock migration tasks related to a sister team’s
+                                task on creating an extensible, server-driven UI framework,
+                                in order to support future maintainability and modularity.<br/></p>
                     </StyledList>
                 </StyledUnorderedList>
             </StyledDiv>
