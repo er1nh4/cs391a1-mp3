e5cf36db3f6edb7ab99cc5999c0ff44388df2af8
Erin Cho <echo@crc-dot1x-nat-10-239-105-199.bu.edu>
2025-10-22 20:18:01 -0400

    Added work exp

--- src/components/main/Employment.tsx
+++ src/components/main/Employment.tsx
@@ -22,9 +22,31 @@ export default function Employment() {
     return (
         <StyledPage id="main-page">
             <StyledDiv>
+                <h2>Employment History</h2>
                 <StyledHeader>
-                    <h3>Match Charter Public High School</h3>
-                    <h4> Face-To-Face Tutor <em> September 2025 - Present </em></h4>
+                    <h3>Match Charter Public High School — Face To Face Tutor</h3>
+                    <h4><em> September 2025 - Present </em></h4>
+                    <p> On top of my studies and supporting-education roles, I'm also
+                        very excited about second-hand fashion, fabric crafts, and jewelry-making.
+                        I love sharing my insights and my work to those around me, and I plan on
+                        carrying these passions with me throughout my professional life!</p>
+
+                    <h3>New York Tutoring Center — Coding Instructor</h3>
+                    <h4><em> May - July 2025 </em></h4>
+                    <p> On top of my studies and supporting-education roles, I'm also
+                        very excited about second-hand fashion, fabric crafts, and jewelry-making.
+                        I love sharing my insights and my work to those around me, and I plan on
+                        carrying these passions with me throughout my professional life!</p>
+
+                    <h3>Google — Software Engineering Intern, STEP</h3>
+                    <h4><em> May - August 2024 </em></h4>
+                    <p> On top of my studies and supporting-education roles, I'm also
+                        very excited about second-hand fashion, fabric crafts, and jewelry-making.
+                        I love sharing my insights and my work to those around me, and I plan on
+                        carrying these passions with me throughout my professional life!</p>
+
+                    <h3>YouTube — Software Engineering Intern, STEP</h3>
+                    <h4><em> June - September 2023 </em></h4>
                     <p> On top of my studies and supporting-education roles, I'm also
                         very excited about second-hand fashion, fabric crafts, and jewelry-making.
                         I love sharing my insights and my work to those around me, and I plan on
